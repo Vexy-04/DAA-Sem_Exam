7a521cff6638a4357596da6b45c30e8cfd7f3d1b
Vexy-04 <varunal2003@gmail.com>
2023-09-08 09:58:05 +0530

    v1.1

--- Part A/1.py
+++ Part A/1.py
@@ -1,2 +1,16 @@
 ## Write a Python program to create an array of 5 integers and display the array items. Access individual elements through indexes.
 
+# Create an array of 5 integers
+array = [int(x) for x in input("Enter the elements of the array, separated by spaces: ").split()]
+
+# Display the array items
+print("Array items:")
+for i in range(len(array)):
+    print(f"Element at index {i}: {array[i]}")
+
+# Array items:
+# Element at index 0: 10
+# Element at index 1: 20
+# Element at index 2: 30
+# Element at index 3: 40
+# Element at index 4: 50
