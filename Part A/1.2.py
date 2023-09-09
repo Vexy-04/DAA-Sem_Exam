52d24cde243524aeb9324b58e872799ae36cc7d9
Vexy-04 <varunal2003@gmail.com>
2023-09-09 06:25:33 +0530

    v1.1

---
+++ Part A/1.2.py
@@ -0,0 +1,26 @@
+array = [int(x) for x in input("Enter the elements of the array, separated by spaces: ").split()]
+print("The array elements are: ",array)
+
+if __name__ == "__main__":
+    while True:
+        index = int(input("Enter the index: "))
+        if index < len(array):
+            print("Element at index", index, "is:", array[index])
+        else:
+            print("Invalid index!")
+        try_again = input("Do you want to try again with different number (y/n):").lower()
+        if try_again != "y":
+            break
+'''
+Enter the elements of the array, separated by spaces: 10 20 30 40 50
+The array elements are:  [10, 20, 30, 40, 50]
+Enter the index: 0
+Element at index 0 is: 10
+Do you want to try again with different number (yes/no):yes
+Enter the index: 4
+Element at index 4 is: 50
+Do you want to try again with different number (yes/no):yes 
+Enter the index: 7
+Invalid index!
+Do you want to try again with different number (yes/no):no
+'''
